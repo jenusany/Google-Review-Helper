d0a8ddbb5b2c8bf0cca5aef9c8be6fbd2e2c6a8d
jenusany <jenusany@outlook.com>
2024-07-11 14:26:49 -0400

    fixed secret and id inputs on auth linkj

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const code = urlParams.get('code');
 
 if (code) {
   // Exchange the code for an access token
-  fetch(`https://graph.facebook.com/v11.0/oauth/access_token?client_id=YOUR_APP_ID&redirect_uri=https://jenusany.github.io/Instagram-comment-analysis&client_secret=YOUR_APP_SECRET&code=${code}`)
+  fetch(`https://graph.facebook.com/v11.0/oauth/access_token?client_id=803246971960239&redirect_uri=https://jenusany.github.io/Instagram-comment-analysis&client_secret=c19298b4ce75926bf2dc0177b77e5912&code=${code}`)
     .then(response => response.json())
     .then(data => {
       const accessToken = data.access_token;
